68ede87d86f3d2e765204d055fbbdfbbd9eef7f0
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-12-30 16:57:04 +0530

    removed comments and installed webpack-dev-server

--- src/appLogic.js
+++ src/appLogic.js
@@ -1,13 +1,3 @@
-// import report from './weatherData';
-
-// const controller = () => {
-//   document.getElementById('form-submit').addEventListener('click', (event) => {
-//     event.preventDefault();
-//     const cityName = document.getElementById('form-text').value;
-//     report(cityName);
-//   });
-// };
-
 const togglebtn = (element = '', temp = '') => {
   let currentUnit = '';
   if (element) {
