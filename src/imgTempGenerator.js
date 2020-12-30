8a4c1d0a4e21cfd630a490d60550017c929c2714
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-12-30 18:48:39 +0530

    fixed npm start issue and landing page set to dist

--- src/imgTempGenerator.js
+++ src/imgTempGenerator.js
@@ -1,30 +1,4 @@
-const getImage = (value) => {
-  let url = '';
-  if (value === 'Smoke') {
-    url = 'images/smoke.gif';
-  } else if (value === 'Mist') {
-    url = 'images/mist.gif';
-  } else if (value === 'Rain') {
-    url = 'images/rain.gif';
-  } else if (value === 'Clear') {
-    url = 'images/clear.gif';
-  } else if (value === 'Clouds') {
-    url = 'images/clouds.gif';
-  } else if (value === 'Snow') {
-    url = 'images/snow.gif';
-  } else if (value === 'Extreme') {
-    url = 'images/extreme.gif';
-  } else if (value === 'Thunderstorm') {
-    url = 'images/thunderstorm.gif';
-  } else if (value === 'Drizzle') {
-    url = 'images/drizzle.gif';
-  } else if (value === 'Haze') {
-    url = 'images/haze.gif';
-  } else if (value === 'Fog') {
-    url = 'images/fog.gif';
-  }
-  return url;
-};
+const getImage = (value) => `images/${value.toLowerCase()}.gif`;
 
 const temperatures = (temp) => {
   for (let i = 0; i < temp[0].kel.length; i += 1) {
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+  },
 };
